411e02054f8e6cbc115d378207ef4d8baacdf0a4
abi <abinav.vanam@gmail.com>
2026-02-20 18:50:24 +0530

    basic jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,33 @@ import './index.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <form className = "new-item-form">
+        <div className = "form-row">
+          <label htmlFor="item" /> Enter Task
+          <input type = "text" id="item" />
+        </div>
+        <button className="btn"> Add </button>
+      </form>
+      <h1 className='header'>To-Do List</h1>
+      <ul className = 'list'>
+        <li>
+          <label>
+            <input type="checkbox" />
+            item 1
+          </label>
+          <button className='btn btn-danger'> Danger </button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            item 2
+          </label>
+          <button className='btn btn-danger'> Danger </button>
+        </li>
+      </ul>
+    </>
   )
 }
 
-export default App
+export default App;
